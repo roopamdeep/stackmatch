4e463542b6aaeefd91f9b0f39e6bed5330e03a2f
roopamdeep <roopam_kaur@yahoo.in>
2026-03-12 14:05:13 -0400

    Initial setup done- homepage, layout, navbar

---
+++ components/NavBar.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+export default function Navbar() {
+  return (
+    <nav className="flex gap-6 p-4 bg-gray-100">
+      <Link href="/">Home</Link>
+      <Link href="/jobs">Jobs</Link>
+      <Link href="/dashboard">Dashboard</Link>
+      <Link href="/signup">Signup</Link>
+      <Link href="/login">Login</Link>
+    </nav>
+  );
+}
